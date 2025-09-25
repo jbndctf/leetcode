589375f53ab7460e2031ed4d2866ba51fc2f20c0
Jonah Benedicto <jbndctf@jbndctf.localdomain>
2025-09-25 12:15:04 +1000

    add two numbers solution in c++

---
+++ c++/2-add-two-numbers.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode(0);
+        ListNode* current = result;
+        int carry_in = 0;
+        while (l1 != nullptr || l2 != nullptr || carry_in != 0) {
+            int n1 = 0;
+            int n2 = 0;
+            if (l1 != nullptr) {
+                n1 = l1->val;
+            }
+            if (l2 != nullptr) {
+                n2 = l2->val;
+            }
+            int sum = n1 + n2 + carry_in;
+            carry_in = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            if (l1 != nullptr) {
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                l2 = l2->next;
+            }
+        }
+        return result->next;
+    }
+};
