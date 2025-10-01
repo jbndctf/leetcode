5eb77bca35bdc2f0b1dacbe322e53e62dbc130cc
jbndctf <jbndctf@gmail.com>
2025-10-01 16:51:25 +1000

    add leetcode 242 solution in c++

---
+++ c++/242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+#include <map>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> freq1;
+        for (char c : s) {
+            freq1[c]++;
+        }
+        map<char, int> freq2;
+        for (char c : t) {
+            freq2[c]++;
+        }
+        if (freq1 == freq2) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+};
